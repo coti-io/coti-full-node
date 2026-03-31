3f8aa4d7451410d004c4a313c430d63e88eabf16
oded neaman <oded.neaman@coti.io>
2026-03-31 10:05:00 +0300

    Update minimum disk space requirement from 100GB to 90GB in install_coti-full-node.sh

--- install_coti-full-node.sh
+++ install_coti-full-node.sh
@@ -26,7 +26,7 @@ if [ -f "$INSTALLER_ENV_FILE" ]; then
 fi
 
 # Default values (can be overridden in installer.env)
-: "${DISK_SPACE_REQUIRED:=100}"      # GB
+: "${DISK_SPACE_REQUIRED:=90}"      # GB
 
 # present the requirements for the installer
 echo "====================================================="
